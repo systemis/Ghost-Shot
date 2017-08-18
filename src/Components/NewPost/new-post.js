331ee6ad0577f1130ff4e7e72af8e35db4b86067
systemis <cuthinh2002@icloud.com>
2017-08-18 12:09:29 +0700

    Building header of create new post group ui :100:

--- src/Components/NewPost/new-post.js
+++ src/Components/NewPost/new-post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import PostPhoto from './post-photo.js';
-import PostStatus from './post-status.js';
+import {connect}            from 'react-redux';
+import PostPhoto            from './post-photo.js';
+import PostStatus           from './post-status.js';
 import './Style/new-post-style.css';
 
 class PostGroup extends Component {
@@ -14,7 +15,11 @@ class PostGroup extends Component {
         }
     }
 
-    
+    exitGroup(){
+        document.getElementById('dialog-field').classList.remove('show');
+        this.props.dispatch({type: 'CHANGE_DIALOG', value: ''});
+    }
+
     addPhoto(){
 
     }
@@ -44,8 +49,24 @@ class PostGroup extends Component {
             <div className="new-post-group">
                 Post group
                 <div className="child-group">
-                    <div className="header">
-                        
+                    <div className="header row">
+                        <div className="show-back-btn">
+                            <button
+                                className="back-btn-screen-new-post-group"
+                                >
+                                <span className="fa fa-chevron-left"></span>
+                            </button>
+                        </div>
+                        <div className="show-title-label">
+                            <p>Create new post </p>
+                        </div>
+                        <div className="show-exit-btn">
+                            <button
+                                className="exit-btn-new-post-group"
+                                onClick={this.exitGroup.bind(this)}>
+                                <span className="fa fa-times"></span>
+                            </button>
+                        </div>
                     </div>
                     <div className="main-layout">
                         {this.state.screens[this.state.index]}
@@ -56,4 +77,8 @@ class PostGroup extends Component {
     }
 }
 
-export default PostGroup;
+export default connect(state => {
+    return {
+        dialogInfo: state.dialogInfo
+    }
+})(PostGroup);
