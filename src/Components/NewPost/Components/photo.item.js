54a774c2dfa567cd0da16a50ef64b8577a7ab7f5
systemis <cuthinh2002@icloud.com>
2017-08-18 17:43:14 +0700

    Building beautiful ui for photo item :100:

--- src/Components/NewPost/Components/photo.item.js
+++ src/Components/NewPost/Components/photo.item.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import ex                   from '../../../image/ex-w-1.png';
 
 class PhotoItem extends Component {
     render() {
         return (
-            <div 
-                className="photo-item"
-                style={{backgroundImage: `url(${this.props.link})`}}>
-                
+            <div className="photo-item col-md-4 col-sm-4 col-xs-4">
+                <div 
+                    className="child-photo-item"
+                    id={`show-image-uploaded-imgur${this.props.index}`}
+                    style={{backgroundImage: `url(${ex})`}}>>
+                </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+        const siItem = document.getElementById(`show-image-uploaded-imgur${this.props.index}`);
+        siItem.style.height = siItem.clientWidth + 'px';
     }
 }
 
--- src/Components/NewPost/post-photo.js
+++ src/Components/NewPost/post-photo.js
@@ -5,22 +5,36 @@ import PhotoPostItem        from './Components/photo-post-item.js';
 class PostPhotoGroup extends Component {
     render() {
         return (
-            <div className="post-photo-group">
+            <div className="post-photo-group row">
                 {this.props.photo.map((link, index) => {
-                    console.log(link);
                     return(
                         <PhotoItem 
-                            key={index} 
+                            key={index}
+                            index={index + 1} 
                             link={link} />
                     )
                 })}
+
+                <div 
+                    id="photo-post-group"
+                    className="col-md-4 col-sm-4 col-xs-4">
+                        <div className="child">
+                            <label htmlFor="uploadFileItem1">Choose a file</label>
+                        </div>
+                </div>
+                
                 <PhotoPostItem 
                     addPhoto={this.props.addPhoto}/>
             </div>
         );
     }
 
-
+    componentDidMount() {
+        document.getElementById('photo-post-group').style.height = 
+        document.getElementById('photo-post-group').clientWidth - 1 + 'px';
+        
+    }
+    
     shouldComponentUpdate(nextProps, nextState) {
         console.log(nextProps.photo);
         return true;
