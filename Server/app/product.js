e98a1848c3da6e63f57ec32ee3ddf8249104a111
systemis <cuthinh2002@icloud.com>
2017-08-19 14:03:53 +0700

    building route to save post :100:

--- Server/app/product.js
+++ Server/app/product.js
@@ -1,6 +1,30 @@
+const postsDM = require('../model/posts.js');
 module.exports = app => {
     app.post(`/new/post/`, (req, res) => {
         console.log(req.user);
         console.log(req.body);
+        if(!req.isAuthenticate()) return res.send({err: 'Not login', result: null});
+
+        const post   = req.body;
+        const user   = req.user;
+        const bundle = {
+            status: post.status,
+            photos: post.photos,
+            date: post.photos,
+            user: {
+                id: user.id,
+                username: username,
+                avatar: user.avatar,
+            }
+        }
+
+        console.log(user);
+        console.log(post);
+        postsDM.newPost(bundle, (error, result) => {
+            console.log(error);
+            console.log(result);
+
+            res.send({err: error, result: result});
+        })
     })
 }
--- Server/model/posts.js
+++ Server/model/posts.js
@@ -1,6 +1,6 @@
 const pool = require('../config/database.js');
 const tableName = `PostsData`
-class postDM{
+class postsDM{
     constructor(){
         pool.query('CREATE TABLE IF NOT EXISTS`PostsData` ( `id` VARCHAR(200) NOT NULL , `user` TEXT NOT NULL , `status` TEXT NOT NULL , `photos` TEXT NOT NULL , `comments` TEXT NULL, `likes` TEXT NULL, `date` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci', (err, result) => {
             console.log(`Error when create table ${tableName}: ${err}`);
@@ -20,7 +20,8 @@ class postDM{
             bundle.id = Math.floor((Math.random() * 2000) + 1).toString();
             this.findById(bundle.id, (error, result) => {
                 if(error){
-                    bool = false;                
+                    bool = false;         
+                    bundle.user     = JSON.stringify(bundle.user);
                     bundle.photos   = JSON.stringify(bundle.photos);
                     bundle.comments = '[]';
                     bundle.likes    = '[]';
@@ -63,4 +64,4 @@ class postDM{
     }
 }
 
-module.exports = new postDM();
+module.exports = new postsDM();
