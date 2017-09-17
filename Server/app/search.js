ad29d604009f4cdb6b564d57f6edbc0e1ef71178
systemis <cuthinh2002@icloud.com>
2017-09-17 16:23:48 +0700

    Fix bug when set prioritize for search result

--- Server/app/search.js
+++ Server/app/search.js
@@ -5,24 +5,49 @@ module.exports = app => {
     const findPeople = (word, clientInfo, searchResult, res) => {
         var userPrioritize = [];
         if(clientInfo){
-            userPrioritize.push(clientInfo.user);
+            userPrioritize.push(clientInfo.username);
             userPrioritize = userPrioritize.concat(clientInfo.following);
+
+            console.log(userPrioritize);
         }   
 
         // Find by client's followings
-        userPrioritize.map((username, index) => {
-            if(username.indexOf('word') >= 0){
-                userDM.findUserByName(username, (error, result) => {
-                    if(!errot && result.length > 0){
-                        searchResult.push(result);
-                    }
-                })
-            }
-        })
-
+        // userPrioritize.map((username, index) => {
+        //     if(username.indexOf('word') >= 0){
+        //         userDM.findUserByName(username, (error, result) => {
+        //             if(!errot && result.length > 0){
+        //                 searchResult.push(result);
+        //             }
+        //         })
+        //     }
+        // })
+        
         userDM.findUsersByName(word, (error, result) => {
             searchResult = searchResult.concat(result);
-            console.log(searchResult);
+            
+            searchResult.map((item, index) => {
+                if(userPrioritize.indexOf(item.username) >= 0){
+                    item.prv = 1;
+                }
+
+                if(clientInfo.username === item.username){
+                    item.prv = 2;
+                }
+            })
+
+            for(var i = 0, length = searchResult.length; i < length; i++){
+                for(var j = length - 1; j > i; j--){
+                    var prv1 = searchResult[j].prv || 0;
+                    var prv2 = searchResult[j - 1].prv || 0;
+
+                    if(prv1 > prv2){
+                        const pg = searchResult[j];
+                        searchResult[j] = searchResult[j - 1];
+                        searchResult[j - 1] = pg;
+                    }
+                }
+            }
+
             res.send({error: null, result: searchResult});
         })
     }
